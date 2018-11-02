a0732e8ef74f7212cc4aeaf7f0127ffa82d0714d
rossanderson75 <ross.anderson@mayden.co.uk>
2018-11-02 18:41:33 +0000

    added scripts for google analytics tracking

--- wp-content/themes/client-base/library/client-helpers.php
+++ wp-content/themes/client-base/library/client-helpers.php
@@ -256,18 +256,40 @@ function mayden_first_paragraph($content)
 add_filter('the_content', 'mayden_first_paragraph');
 
 
-/**
- * This tells Contact Form 7 to run the ga() (Google Analytics tracker)
- * function when a form submission completes and mail has been sent successfully.
+/*
+ * Adds Google analytics tracking code to header
  */
-function mycustom_wp_footer() {
+function add_google_analytics() {
+    ?>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-37505502-1', 'auto');
+ga('send', 'pageview');
+
+</script>
+    <?php
+}
+add_action( 'wp_head', 'add_google_analytics' );
+
+
+/**
+ * Runs Google Analytics tracking code after succesfull
+ * form submission with ContactForm7.
+ * Allowing form submissions to be tracked.
+ */
+function contact_form_ga_tracker() {
     ?>
     <script type="text/javascript">
-        document.addEventListener( 'wpcf7mailsent', function( event ) {
+
+        if (document.getElementsByClassName("wpcf7-mail-sent-ok").length) {
             ga( 'send', 'event', 'Contact Form', 'submit' );
-        }, false );
+        }
+
     </script>
     <?php
 }
-
-add_action( 'wp_footer', 'mycustom_wp_footer' );
+add_action( 'wp_footer', 'contact_form_ga_tracker' );
